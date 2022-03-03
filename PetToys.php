d51de92e63b5e40d244f653dc8980e3217c5ef95
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-03-03 15:40:02 +0100

    created PetToys.php

---
+++ PetToys.php
@@ -0,0 +1,48 @@
+<?php
+
+class PetToys
+{
+    public $name_toys;
+    public $type_toys_animal;
+    public $price;
+
+    public function __construct($name_toys, $type_toys_animal, $price)
+    {
+        $this->setNameToys($name_toys);
+        $this->setTypeToysAnimal($type_toys_animal);
+        $this->setPrice($price);
+    }
+
+    public function setNameToys($name_toys)
+    {
+        if (!is_numeric($name_toys)) return
+            $this->name_toys = $name_toys;
+    }
+
+    public function getNameToys()
+    {
+        return $this->name_toys;
+    }
+
+    public function setTypeToysAnimal($type_toys_animal)
+    {
+        if (!is_numeric($type_toys_animal)) return
+            $this->type_toys_animal = $type_toys_animal;
+    }
+
+    public function getTypeToysAnimal()
+    {
+        return $this->type_toys_animal;
+    }
+
+    public function setPrice($price)
+    {
+        if (is_numeric($price) || $price <= 0) return
+            $this->price = $price;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
